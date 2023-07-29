7da8bea791693617f25dd8446908cf8442c45af3
Brianna Farinango <briannafarinango3@gmail.com>
2023-07-29 16:11:13 -0500

    Initial Commit

---
+++ containsDuplicate/main.py
@@ -0,0 +1,11 @@
+# Given an integer array nums, 
+# return true if any value appears at least twice in the array,
+# return false if every element is distinct.
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+
+
+if __name__ == '__main__':
+    print(Solution().containsDuplicate())
+
